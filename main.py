0d71b4caee206b9284c26dca866e96c422d1aaa9
apsyhea <apsyhea@gmail.com>
2023-02-15 18:53:33 +0000

    replace bot token

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import Message
 from aiogram.dispatcher.filters import Text, Command
 
-API_TOKEN = tokens.test_token
+API_TOKEN = tokens.bot_token
 W_TOKEN = tokens.weather_token
 C_TOKEN = tokens.cur_token
 
