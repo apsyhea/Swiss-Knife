4ef6f0dc04158bb47eb6b57ad297696452bd4ac7
apsyhea <apsyhea@gmail.com>
2023-02-22 22:04:08 +0200

    add handle_all_messages

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import logging
 import messages
 from aiogram import Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.types import Message
-from aiogram.dispatcher.filters import Text, Command
-from aiogram.utils.exceptions import InvalidQueryID
+from aiogram.types import Message, ChatType
+from aiogram.dispatcher.filters import Text, Command, ChatTypeFilter
+from aiogram.utils.exceptions import InvalidQueryID, MessageTextIsEmpty
 from tokens import bot_token, weather_token, cur_token
 from warmon import warmon
 from weather import weather
@@ -25,15 +25,20 @@ dp = Dispatcher(bot, storage=storage)
 async def errors_handler(update, exception) -> str:
     if isinstance(exception, InvalidQueryID):
         await update.message.answer("Command not recognized. Please try again.")
-        return
-    raise exception
+    elif isinstance(exception, MessageTextIsEmpty):
+        await update.message.answer("Please provide a valid message.")
+    else:
+        await update.message.answer("An error has occurred.")
+    return
 
 @dp.message_handler(Text(equals=["/start"], ignore_case=True))
 async def cmd_start(message: Message) -> str:
-    dp.register_message_handler(weather, commands=['weather'])
-    dp.register_message_handler(currency, commands=['currency'])
-    dp.register_message_handler(warmon, commands=['warmon'])
-    dp.register_message_handler(iplocate, commands=['iplocate'])
+
+    await message.answer(messages.msg_start,parse_mode="HTML")
+
+@dp.message_handler(Text(equals=["/help"], ignore_case=True))
+async def cmd_help(message: Message) -> str:
+
     await message.answer(messages.msg_start,parse_mode="HTML")
 
 @dp.message_handler(Command("weather"))
@@ -52,6 +57,16 @@ async def cmd_warmon(message: Message) -> str:
 @dp.message_handler(Command("iplocate"))
 async def cmd_iplocate(message: Message) -> str:
     await iplocate(message)
+
+@dp.message_handler(ChatTypeFilter(ChatType.PRIVATE), content_types=['text'])
+async def handle_all_messages(message: Message):
+    await message.answer("Sorry, I don't understand. Try /help")
+
+dp.register_message_handler(cmd_weather, ChatTypeFilter(ChatType.PRIVATE))
+dp.register_message_handler(cmd_currency, ChatTypeFilter(ChatType.PRIVATE))
+dp.register_message_handler(cmd_warmon, ChatTypeFilter(ChatType.PRIVATE))
+dp.register_message_handler(cmd_iplocate, ChatTypeFilter(ChatType.PRIVATE))
+dp.register_message_handler(handle_all_messages, ChatTypeFilter(ChatType.PRIVATE))
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
