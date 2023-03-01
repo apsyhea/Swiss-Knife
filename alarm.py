8e2588b7b44e110b1241f7bfdf4caef726b2fc95
APSYHEA <apsyhea@gmail.com>
2023-03-01 11:03:30 +0200

    english

--- alarm.py
+++ alarm.py
@@ -7,7 +7,6 @@ from aiogram.types import Message
 
 A_TOKEN = alarm_token
 
-
 async def alarm(message: Message):
     url = 'https://alerts.com.ua/api/states'
     headers = {'X-API-Key': f'{A_TOKEN}'}
@@ -22,6 +21,8 @@ async def alarm(message: Message):
             state = json.loads(data)
             for alarm_state in state['states']:
                 if alarm_state['alert']:
-                    alert_names.append(alarm_state['name'])
+                    alert_names.append(alarm_state['name_en'])
+                    alarm_state['changed'] = alarm_state.get('changed', '')  # add 'changed' field if not present
             if alert_names:
-                await message.reply("<b>⚠️ Повітрянна тривога:\n🚨 </b>"+"\n🚨 ".join(alert_names), parse_mode='HTML')
+                alarm_info = "\n🚨 ".join(alert_names)
+                await message.reply(f"<b>⚠️ Air alarm:\n🚨 {alarm_info} </b>", parse_mode='HTML')
