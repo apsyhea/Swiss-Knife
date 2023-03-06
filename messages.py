b54b6f9624d96ce1a946caebbb5557d3aea91696
apsyhea <apsyhea@gmail.com>
2023-03-06 22:54:46 +0200

    hot fix messages

--- messages.py
+++ messages.py
@@ -1,13 +1,13 @@
 msg_start: str = """
 <b>Hello, I'm a Swiss Knife bot. 
 \nTo display the current weather 
-type: /weather [city] 
+input: /weather [city] 
 For example <code>/weather Tokyo</code>
 \nTo convert the exchange rate 
-type: /currency [number][currency][currency]
+input: /currency [number][currency][currency]
 For example:  <code>/currency 1 eur usd</code>
 \nFind out the geo and provider ip address:
-type: /iplocate [ip address]
+input: /iplocate [ip address]
 For example: <code>/iplocate 8.8.8.8</code>
 \nUse /warmon to display rashist casualty statistics for the day</b>
 \nUse /alarm to display air alarm in Ukraine
