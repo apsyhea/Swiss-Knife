8eda1160a222163474830c0e786932a89eb4b883
apsyhea <apsyhea@gmail.com>
2023-03-07 00:15:47 +0200

    fix

--- blocklist.py
+++ blocklist.py
@@ -1,40 +1,28 @@
 import json
 import aiohttp
-import asyncio
 from aiogram import types
 from aiogram.types import ParseMode
 
 
-# Load blocklist from API
 async def load_blocklist():
     async with aiohttp.ClientSession() as session:
         async with session.get("https://uablacklist.net/all.json") as response:
             data = await response.text()
     return json.loads(data)
 
-async def main():
-    blocklist = await load_blocklist()
-#    print(blocklist)
 
-asyncio.run(main())
-
-
-# Check if a given IP or URL is in the blocklist
 async def is_blocked(ip_or_url):
+    blocklist = await load_blocklist()
     for item in blocklist:
-        
-        if ip_or_url in item["ips"] or ip_or_url in item["urls"]:
+        if isinstance(item, dict) and (ip_or_url in item["ips"] or ip_or_url in item["urls"]):
             return item
-        
     return None
 
-# Handle /blocklist command
 
 async def blocklist(message: types.Message):
-    text = message.text.replace("/blocklist ", "")
+    text = message.get_args()
     item = await is_blocked(text)
     if item:
-        # Display blocklist info
         alias = item["alias"]
         term = item["term"]
         urls = "\n".join(item["urls"])
@@ -42,6 +30,5 @@ async def blocklist(message: types.Message):
         msg = f"<b>{alias}</b> is blocked in Ukraine until {term}.\n\nURLs:\n{urls}\n\nIPs:\n{ips}"
         await message.reply(msg, parse_mode=ParseMode.HTML)
     else:
-        # Display not blocked message
         msg = f"{text} is not blocked in Ukraine."
         await message.reply(msg)
